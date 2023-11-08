1d022e820bf6ac445e0aca3d52b6b0aea80c0ca1
isamir <samir.ibrahim38@outlook.fr>
2023-11-08 15:53:20 +0100

    quasi fin javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,81 @@
-console.log("YOUHOOOO");
+console.log("Youhoooo !");
+console.log(" ");
 
-let dataAdressFetch;
-const urlApiBase = "https://api-adresse.data.gouv.fr/search/?q=";
-// const urlApiAdresse = urlApiBase + adresse;
+let rueFacturation = document.getElementById("streetFactur");
+let villeFacturation = document.getElementById("cityFactur");
+let codePostalFacturation = document.getElementById("postodeFactur");
 
+rueFacturation.addEventListener("input", async ()  => {
 
+    let dataAdresseFetch;
+    // Je récupère l'adresse de base qui ne changera jamais
+    const urlApiBase = "https://api-adresse.data.gouv.fr/search/?q=";
 
+    // Je récupère ce que va rentrer l'utilisateur dans le rue
+    let valeurEntreeRueUtilisateur = rueFacturation.value;
+    console.log("C'est ce que rentre l'utilisateur dans l'input rue", valeurEntreeRueUtilisateur);
+    console.log(" ");
 
-let adresse = document.getElementById("streetFactur");
-adresse.addEventListener("input", async function () {
+    // Je récupère mes données entrées par l'utilisateur et je les remplace  espaces par des + 
+    let valeurEntreeRueUtilisateurModifiee = valeurEntreeRueUtilisateur.replace(/ /g,'+');
+    // Je crée mon adresse pour mon API finale (j'assemble l'API de base et mes données de l'utilsateur pour avoir l'url exacte)
+    let urlApiAdresse = urlApiBase + valeurEntreeRueUtilisateurModifiee + "&limit=5";
+    console.log('salam url api :',urlApiAdresse);
 
-    let valeurUtilisateur = adresse.value;
-    console.log("Mon utilisateur à entrée : ", valeurUtilisateur);
-
-    // let urlApiAdresse = urlApiBase + valeurUtilisateur;
-    // console.log("urlApiAdresse : ",urlApiAdresse);
-    
-
-    let valeurUtilisateurModifiee = valeurUtilisateur.replace(/ /g, '+');
-    let urlApiAdresse = urlApiBase + valeurUtilisateurModifiee + "&limit=5";
-    console.log("valeurUtilisateur : ", valeurUtilisateur);
-    console.log("valeurUtilisateurModifiee : ", valeurUtilisateurModifiee);
-    console.log("C'est mon url de base : ",urlApiBase);
-    console.log("C'est ma vrai URL qui m'amène vers la bonne API : ",urlApiAdresse);
-   
-    await getDataAdressFetch();
-    async function  getDataAdressFetch() {
-    const res = await fetch(urlApiAdresse);
-    dataAdressFetch = await res.json();
-    console.log('mon dataFetch : ',dataAdressFetch);
-    dataAdressFetch.features.forEach(adresse => {
-        let propriete = adresse.properties;
-
-        let rue = document.getElementById("streetFactur")/*.value*/;
-        let ville = document.getElementById("cityFactur").value;
-        let codePostal = document.getElementById("postcodeFactur")/*.value*/;
-
-        // rue.innerText = propriete.name;
-        // ville.innerText = propriete.city;
-        // codePostal.innerText = propriete.postcode
-
-        console.log("test nom dans le rue", rue.value);
-        console.log("test rue", rue );
-        console.log("test ville",ville);
-        console.log("test ville dans le ville ",ville.value);
-
-
-        console.log("j'essaie de récuperer le ?label? (label) :  ", propriete.label); 
-        console.log("j'essaie de récuperer le nom de la rue ?name? (name) : ", propriete.name);
-        console.log("j'essaie de récuperer la ville (city) : ", propriete.city);  
-        console.log("j'essaie de récuperer le code postal (postcode) : ", propriete.postcode);
-    });
+    // Je récupère mes données de mon API via fetch
+    await getdataAdresseFetch();
+    async function getdataAdresseFetch() {
+        const res = await fetch(urlApiAdresse);
+        dataAdresseFetch = await res.json();
     }
+    console.log("C'est mes données récupéré via Fetch (mon dataFetch) :",dataAdresseFetch);
+    console.log(" ");
+
+    let suggestions = dataAdresseFetch.features
+    console.log('mes suggestions/features : ',suggestions);
+    console.log(" ");
+
+    suggestions.forEach(features => {
+
+        // Je crée une variable suggestion pour avoir directement mes suggestions suivi de ce que je voudrais (ville,rue,code postal)
+        let properties = features.properties
+
+        // Je fais un console log juste pour savoir quel propriétés correspond à quoi
+        console.log("je regarde c'est quoi mes propietes", properties);
+        console.log("j'essaie de récuperer le ?label? (label) :  ", properties.label); 
+        console.log("j'essaie de récuperer le nom de la rue ?name? (name) : ", properties.name);
+        console.log("j'essaie de récuperer la ville (city) : ", properties.city);
+        console.log("j'essaie de récuperer le code postal (postcode) : ", properties.postcode);
+        console.log(" ");
+
+        // // Je récupère mon label de mon input Rue facturation
+        let datalistSelect = document.querySelector("div label datalist")
+        console.log("ma liste :",datalistSelect);
+
+        // Je met mon option en tant qu'enfant de label pour pouvoir afficher mes options dans mon input Rue texte
+        let option = document.createElement("option")
+        datalistSelect.appendChild(option)
+
+        console.log("C'est mon input Rue facturation :", rueFacturation);
+        console.log("C'est la valeur de mon input Rue facturation (ce qu'il y a dans mon input) :", rueFacturation.value);
+        console.log('c\'est juste mon option :', option);
+        console.log('c\'est ce qu\'il y a dans mon option :', option.value);
+        console.log(" ");
+
+        option.textContent = features.properties.label
+
+        // rueFacturation.value = features.properties.name
+        // villeFacturation.value = features.properties.city
+        // codePostalFacturation.value = features.properties.postcode
+        // villeFacturation.value.innerHTML = "",
+
+        console.log("c'est mon input ville :", villeFacturation);
+
+
+
+    });
+
 })
+
 
 
